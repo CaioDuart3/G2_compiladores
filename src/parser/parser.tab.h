e2dc087cc529393a0e01a3de4668dbac489af1d2
felixlaryssa <felixllaryssa@gmail.com>
2025-09-23 22:19:40 -0300

    Parser sintatico
    Co-authored-by: Ludmila <231026750@aluno.unb.br>

---
+++ src/parser/parser.tab.h
@@ -0,0 +1,117 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PARSER_TAB_H_INCLUDED
+# define YY_YY_PARSER_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    TOKEN_IDENTIFICADOR = 258,     /* TOKEN_IDENTIFICADOR  */
+    TOKEN_STRING = 259,            /* TOKEN_STRING  */
+    TOKEN_INTEIRO = 260,           /* TOKEN_INTEIRO  */
+    TOKEN_FLOAT = 261,             /* TOKEN_FLOAT  */
+    TOKEN_PALAVRA_CHAVE_IF = 262,  /* TOKEN_PALAVRA_CHAVE_IF  */
+    TOKEN_PALAVRA_CHAVE_ELSE = 263, /* TOKEN_PALAVRA_CHAVE_ELSE  */
+    TOKEN_PALAVRA_CHAVE_ELIF = 264, /* TOKEN_PALAVRA_CHAVE_ELIF  */
+    TOKEN_PALAVRA_CHAVE_WHILE = 265, /* TOKEN_PALAVRA_CHAVE_WHILE  */
+    TOKEN_PALAVRA_CHAVE_FOR = 266, /* TOKEN_PALAVRA_CHAVE_FOR  */
+    TOKEN_PALAVRA_CHAVE_DEF = 267, /* TOKEN_PALAVRA_CHAVE_DEF  */
+    TOKEN_PALAVRA_CHAVE_RETURN = 268, /* TOKEN_PALAVRA_CHAVE_RETURN  */
+    TOKEN_PALAVRA_CHAVE_IN = 269,  /* TOKEN_PALAVRA_CHAVE_IN  */
+    TOKEN_OPERADOR_IGUAL = 270,    /* TOKEN_OPERADOR_IGUAL  */
+    TOKEN_OPERADOR_DIFERENTE = 271, /* TOKEN_OPERADOR_DIFERENTE  */
+    TOKEN_OPERADOR_MENOR_IGUAL = 272, /* TOKEN_OPERADOR_MENOR_IGUAL  */
+    TOKEN_OPERADOR_MAIOR_IGUAL = 273, /* TOKEN_OPERADOR_MAIOR_IGUAL  */
+    TOKEN_OPERADOR_MENOR = 274,    /* TOKEN_OPERADOR_MENOR  */
+    TOKEN_OPERADOR_MAIOR = 275,    /* TOKEN_OPERADOR_MAIOR  */
+    TOKEN_OPERADOR_ATRIBUICAO = 276, /* TOKEN_OPERADOR_ATRIBUICAO  */
+    TOKEN_OPERADOR_MAIS = 277,     /* TOKEN_OPERADOR_MAIS  */
+    TOKEN_OPERADOR_MENOS = 278,    /* TOKEN_OPERADOR_MENOS  */
+    TOKEN_OPERADOR_MULTIPLICACAO = 279, /* TOKEN_OPERADOR_MULTIPLICACAO  */
+    TOKEN_OPERADOR_DIVISAO = 280,  /* TOKEN_OPERADOR_DIVISAO  */
+    TOKEN_DELIMITADOR_DOIS_PONTOS = 281, /* TOKEN_DELIMITADOR_DOIS_PONTOS  */
+    TOKEN_DELIMITADOR_VIRGULA = 282, /* TOKEN_DELIMITADOR_VIRGULA  */
+    TOKEN_DELIMITADOR_ABRE_PARENTESES = 283, /* TOKEN_DELIMITADOR_ABRE_PARENTESES  */
+    TOKEN_DELIMITADOR_FECHA_PARENTESES = 284, /* TOKEN_DELIMITADOR_FECHA_PARENTESES  */
+    TOKEN_DELIMITADOR_ABRE_COLCHETES = 285, /* TOKEN_DELIMITADOR_ABRE_COLCHETES  */
+    TOKEN_DELIMITADOR_FECHA_COLCHETES = 286, /* TOKEN_DELIMITADOR_FECHA_COLCHETES  */
+    TOKEN_DELIMITADOR_ABRE_CHAVES = 287, /* TOKEN_DELIMITADOR_ABRE_CHAVES  */
+    TOKEN_DELIMITADOR_FECHA_CHAVES = 288 /* TOKEN_DELIMITADOR_FECHA_CHAVES  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 12 "parser.y"
+
+    char* str;
+    int inteiro;
+    float flutuante;
+
+#line 103 "parser.tab.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_PARSER_TAB_H_INCLUDED  */
--- src/parser/parser.y
+++ src/parser/parser.y
@@ -1,24 +1,169 @@
-/* ANALISADOR SINTÁTICO */
 %{
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+FILE *saida;
 
 int yylex(void);
 void yyerror(const char *s);
 %}
 
-%token NUM PLUS MINUS TIMES DIVIDE LPAREN RPAREN
+%union {
+    char* str;
+    int inteiro;
+    float flutuante;
+}
+
+%token <str> TOKEN_IDENTIFICADOR TOKEN_STRING
+%token <inteiro> TOKEN_INTEIRO
+%token <flutuante> TOKEN_FLOAT
+
+
+%token TOKEN_PALAVRA_CHAVE_IF TOKEN_PALAVRA_CHAVE_ELSE TOKEN_PALAVRA_CHAVE_ELIF
+%token TOKEN_PALAVRA_CHAVE_WHILE TOKEN_PALAVRA_CHAVE_FOR
+%token TOKEN_PALAVRA_CHAVE_DEF TOKEN_PALAVRA_CHAVE_RETURN TOKEN_PALAVRA_CHAVE_IN
+
+%token TOKEN_OPERADOR_IGUAL TOKEN_OPERADOR_DIFERENTE
+%token TOKEN_OPERADOR_MENOR_IGUAL TOKEN_OPERADOR_MAIOR_IGUAL
+%token TOKEN_OPERADOR_MENOR TOKEN_OPERADOR_MAIOR
+%token TOKEN_OPERADOR_ATRIBUICAO
+%token TOKEN_OPERADOR_MAIS TOKEN_OPERADOR_MENOS TOKEN_OPERADOR_MULTIPLICACAO TOKEN_OPERADOR_DIVISAO
+
+%token TOKEN_DELIMITADOR_DOIS_PONTOS TOKEN_DELIMITADOR_VIRGULA
+%token TOKEN_DELIMITADOR_ABRE_PARENTESES TOKEN_DELIMITADOR_FECHA_PARENTESES
+%token TOKEN_DELIMITADOR_ABRE_COLCHETES TOKEN_DELIMITADOR_FECHA_COLCHETES
+%token TOKEN_DELIMITADOR_ABRE_CHAVES TOKEN_DELIMITADOR_FECHA_CHAVES
+
+%left TOKEN_OPERADOR_MAIS TOKEN_OPERADOR_MENOS
+%left TOKEN_OPERADOR_MULTIPLICACAO TOKEN_OPERADOR_DIVISAO
+%right TOKEN_OPERADOR_ATRIBUICAO
+
+%type <str> expr
 
 %%
 
-expressao:
-    expressao PLUS expressao
-  | expressao MINUS expressao
-  | expressao TIMES expressao
-  | expressao DIVIDE expressao
-  | LPAREN expressao RPAREN
-  | NUM
-  ;
+program:
+    { fprintf(saida, "#include <stdio.h>\n\nint main() {\n"); }
+    stmt_list
+    { fprintf(saida, "return 0;\n}\n"); }
+;
+
+stmt_list:
+    stmt_list stmt
+    | stmt
+;
+
+stmt:
+    simple_stmt
+    | compound_stmt
+;
+
+simple_stmt:
+    expr_stmt
+    | return_stmt
+;
+
+expr_stmt:
+    TOKEN_IDENTIFICADOR TOKEN_OPERADOR_ATRIBUICAO expr {
+        fprintf(saida, "%s = %s;\n", $1, $3);
+    }
+;
+
+return_stmt:
+    TOKEN_PALAVRA_CHAVE_RETURN expr {
+        fprintf(saida, "return %s;\n", $2);
+    }
+;
+
+compound_stmt:
+    if_stmt
+    | while_stmt
+    | func_def
+;
+
+if_stmt:
+    TOKEN_PALAVRA_CHAVE_IF expr TOKEN_DELIMITADOR_DOIS_PONTOS stmt_list {
+        fprintf(saida, "if (%s) {\n", $2);
+    }
+    TOKEN_PALAVRA_CHAVE_ELSE TOKEN_DELIMITADOR_DOIS_PONTOS stmt_list {
+        fprintf(saida, "} else {\n");
+    }
+    { fprintf(saida, "}\n"); }
+;
+
+while_stmt:
+    TOKEN_PALAVRA_CHAVE_WHILE expr TOKEN_DELIMITADOR_DOIS_PONTOS stmt_list {
+        fprintf(saida, "while (%s) {\n", $2);
+        fprintf(saida, "}\n");
+    }
+;
+
+func_def:
+    TOKEN_PALAVRA_CHAVE_DEF TOKEN_IDENTIFICADOR TOKEN_DELIMITADOR_ABRE_PARENTESES opt_params TOKEN_DELIMITADOR_FECHA_PARENTESES TOKEN_DELIMITADOR_DOIS_PONTOS stmt_list {
+        fprintf(saida, "int %s(", $2);
+        fprintf(saida, ") {\n");
+        fprintf(saida, "}\n");
+    }
+;
+
+opt_params:
+    /* vazio */
+    | param_list
+;
+
+param_list:
+    TOKEN_IDENTIFICADOR {
+        fprintf(saida, "int %s", $1);
+    }
+    | param_list TOKEN_DELIMITADOR_VIRGULA TOKEN_IDENTIFICADOR {
+        fprintf(saida, ", int %s", $3);
+    }
+;
+
+expr:
+    expr TOKEN_OPERADOR_MAIS expr {
+        char *buf = malloc(strlen($1) + strlen($3) + 6);
+        sprintf(buf, "(%s + %s)", $1, $3);
+        $$ = buf;
+    }
+    | expr TOKEN_OPERADOR_MENOS expr {
+        char *buf = malloc(strlen($1) + strlen($3) + 6);
+        sprintf(buf, "(%s - %s)", $1, $3);
+        $$ = buf;
+    }
+    | expr TOKEN_OPERADOR_MULTIPLICACAO expr {
+        char *buf = malloc(strlen($1) + strlen($3) + 6);
+        sprintf(buf, "(%s * %s)", $1, $3);
+        $$ = buf;
+    }
+    | expr TOKEN_OPERADOR_DIVISAO expr {
+        char *buf = malloc(strlen($1) + strlen($3) + 6);
+        sprintf(buf, "(%s / %s)", $1, $3);
+        $$ = buf;
+    }
+    | TOKEN_DELIMITADOR_ABRE_PARENTESES expr TOKEN_DELIMITADOR_FECHA_PARENTESES {
+        char *buf = malloc(strlen($2) + 3);
+        sprintf(buf, "(%s)", $2);
+        $$ = buf;
+    }
+    | TOKEN_INTEIRO {
+        char *buf = malloc(32);
+        sprintf(buf, "%d", $1);
+        $$ = buf;
+    }
+    | TOKEN_FLOAT {
+        char *buf = malloc(32);
+        sprintf(buf, "%f", $1);
+        $$ = buf;
+    }
+    | TOKEN_STRING {
+        $$ = $1;
+    }
+    | TOKEN_IDENTIFICADOR {
+        $$ = $1;
+    }
+;
 
 %%
 
@@ -27,6 +172,12 @@ void yyerror(const char *s) {
 }
 
 int main(void) {
+    saida = fopen("output.c", "w");
+    if (!saida) {
+        perror("Erro ao abrir output.c");
+        return 1;
+    }
     yyparse();
+    fclose(saida);
     return 0;
 }
